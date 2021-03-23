0f2480c91dc39650ae5a1442e1dfc6d5b3e08ba9
Shinichi Haramizu <shinichi@haramizu.jp>
2021-03-23 09:10:36 +0900

    lighthouse

---
+++ 101/XP0-SingleDeveloper.ps1
@@ -0,0 +1,213 @@
+# The Prefix that will be used on SOLR, Website and Database instances.
+$Prefix = "XP0"
+# The Password for the Sitecore Admin User. This will be regenerated if left on the default.
+$SitecoreAdminPassword = "b"
+# The root folder with the license file and WDP files.
+$SCInstallRoot = "C:\projects\sif"
+# Root folder to install the site to. If left on the default [systemdrive]:\\inetpub\\wwwroot will be used
+$SitePhysicalRoot = ""
+# The name for the XConnect service.
+$XConnectSiteName = "$prefix.xconnect"
+# The Sitecore site instance name.
+$SitecoreSiteName = "$prefix.sc"
+# Identity Server site name
+$IdentityServerSiteName = "$prefix.identityserver"
+# The Path to the license file
+$LicenseFile = "$SCInstallRoot\license.xml"
+# The URL of the Solr Server
+$SolrUrl = "https://localhost:8983/solr"
+# The Folder that Solr has been installed to.
+$SolrRoot = "C:\Solr-8.4.0"
+# The Name of the Solr Service.
+$SolrService = "Solr-8.4.0"
+# The DNS name or IP of the SQL Instance.
+$SqlServer = "localhost"
+# A SQL user with sysadmin privileges.
+$SqlAdminUser = "sa"
+# The password for $SQLAdminUser.
+$SqlAdminPassword = "Pleasechang3p@ssword"
+# The path to the XConnect Package to Deploy.
+$XConnectPackage = (Get-ChildItem "$SCInstallRoot\Sitecore * rev. * (OnPrem)_xp0xconnect.scwdp.zip").FullName
+# The path to the Sitecore Package to Deploy.
+$SitecorePackage = (Get-ChildItem "$SCInstallRoot\Sitecore * rev. * (OnPrem)_single.scwdp.zip").FullName
+# The path to the Identity Server Package to Deploy.
+$IdentityServerPackage = (Get-ChildItem "$SCInstallRoot\Sitecore.IdentityServer * rev. * (OnPrem)_identityserver.scwdp.zip").FullName
+# The Identity Server password recovery URL, this should be the URL of the CM Instance
+$PasswordRecoveryUrl = "https://$SitecoreSiteName"
+# The URL of the Identity Server
+$SitecoreIdentityAuthority = "https://$IdentityServerSiteName"
+# The URL of the XconnectService
+$XConnectCollectionService = "https://$XConnectSiteName"
+# The random string key used for establishing connection with IdentityService. This will be regenerated if left on the default.
+$ClientSecret = "SIF-Default"
+# Pipe-separated list of instances (URIs) that are allowed to login via Sitecore Identity.
+$AllowedCorsOrigins = "https://$SitecoreSiteName"
+
+
+# Install XP0 via combined partials file.
+$singleDeveloperParams = @{
+    Path = "$SCInstallRoot\XP0-SingleDeveloper.json"
+    SqlServer = $SqlServer
+    SqlAdminUser = $SqlAdminUser
+    SqlAdminPassword = $SqlAdminPassword
+    SitecoreAdminPassword = $SitecoreAdminPassword
+    SolrUrl = $SolrUrl
+    SolrRoot = $SolrRoot
+    SolrService = $SolrService
+    Prefix = $Prefix
+    XConnectCertificateName = $XConnectSiteName
+    IdentityServerCertificateName = $IdentityServerSiteName
+    IdentityServerSiteName = $IdentityServerSiteName
+    LicenseFile = $LicenseFile
+    XConnectPackage = $XConnectPackage
+    SitecorePackage = $SitecorePackage
+    IdentityServerPackage = $IdentityServerPackage
+    XConnectSiteName = $XConnectSiteName
+    SitecoreSitename = $SitecoreSiteName
+    PasswordRecoveryUrl = $PasswordRecoveryUrl
+    SitecoreIdentityAuthority = $SitecoreIdentityAuthority
+    XConnectCollectionService = $XConnectCollectionService
+    ClientSecret = $ClientSecret
+    AllowedCorsOrigins = $AllowedCorsOrigins
+    SitePhysicalRoot = $SitePhysicalRoot
+}
+
+Push-Location $SCInstallRoot
+
+Install-SitecoreConfiguration @singleDeveloperParams *>&1 | Tee-Object XP0-SingleDeveloper.log
+
+# Uncomment the below line and comment out the above if you want to remove the XP0 SingleDeveloper Config
+#Uninstall-SitecoreConfiguration @singleDeveloperParams *>&1 | Tee-Object XP0-SingleDeveloper-Uninstall.log
+
+Pop-Location
+
+# SIG # Begin signature block
+# MIIXzQYJKoZIhvcNAQcCoIIXvjCCF7oCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUeGAz+NczayJIZTbPRKZxRFAQ
+# Y8GgghMIMIID7jCCA1egAwIBAgIQfpPr+3zGTlnqS5p31Ab8OzANBgkqhkiG9w0B
+# AQUFADCBizELMAkGA1UEBhMCWkExFTATBgNVBAgTDFdlc3Rlcm4gQ2FwZTEUMBIG
+# A1UEBxMLRHVyYmFudmlsbGUxDzANBgNVBAoTBlRoYXd0ZTEdMBsGA1UECxMUVGhh
+# d3RlIENlcnRpZmljYXRpb24xHzAdBgNVBAMTFlRoYXd0ZSBUaW1lc3RhbXBpbmcg
+# Q0EwHhcNMTIxMjIxMDAwMDAwWhcNMjAxMjMwMjM1OTU5WjBeMQswCQYDVQQGEwJV
+# UzEdMBsGA1UEChMUU3ltYW50ZWMgQ29ycG9yYXRpb24xMDAuBgNVBAMTJ1N5bWFu
+# dGVjIFRpbWUgU3RhbXBpbmcgU2VydmljZXMgQ0EgLSBHMjCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBALGss0lUS5ccEgrYJXmRIlcqb9y4JsRDc2vCvy5Q
+# WvsUwnaOQwElQ7Sh4kX06Ld7w3TMIte0lAAC903tv7S3RCRrzV9FO9FEzkMScxeC
+# i2m0K8uZHqxyGyZNcR+xMd37UWECU6aq9UksBXhFpS+JzueZ5/6M4lc/PcaS3Er4
+# ezPkeQr78HWIQZz/xQNRmarXbJ+TaYdlKYOFwmAUxMjJOxTawIHwHw103pIiq8r3
+# +3R8J+b3Sht/p8OeLa6K6qbmqicWfWH3mHERvOJQoUvlXfrlDqcsn6plINPYlujI
+# fKVOSET/GeJEB5IL12iEgF1qeGRFzWBGflTBE3zFefHJwXECAwEAAaOB+jCB9zAd
+# BgNVHQ4EFgQUX5r1blzMzHSa1N197z/b7EyALt0wMgYIKwYBBQUHAQEEJjAkMCIG
+# CCsGAQUFBzABhhZodHRwOi8vb2NzcC50aGF3dGUuY29tMBIGA1UdEwEB/wQIMAYB
+# Af8CAQAwPwYDVR0fBDgwNjA0oDKgMIYuaHR0cDovL2NybC50aGF3dGUuY29tL1Ro
+# YXd0ZVRpbWVzdGFtcGluZ0NBLmNybDATBgNVHSUEDDAKBggrBgEFBQcDCDAOBgNV
+# HQ8BAf8EBAMCAQYwKAYDVR0RBCEwH6QdMBsxGTAXBgNVBAMTEFRpbWVTdGFtcC0y
+# MDQ4LTEwDQYJKoZIhvcNAQEFBQADgYEAAwmbj3nvf1kwqu9otfrjCR27T4IGXTdf
+# plKfFo3qHJIJRG71betYfDDo+WmNI3MLEm9Hqa45EfgqsZuwGsOO61mWAK3ODE2y
+# 0DGmCFwqevzieh1XTKhlGOl5QGIllm7HxzdqgyEIjkHq3dlXPx13SYcqFgZepjhq
+# IhKjURmDfrYwggSjMIIDi6ADAgECAhAOz/Q4yP6/NW4E2GqYGxpQMA0GCSqGSIb3
+# DQEBBQUAMF4xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jwb3Jh
+# dGlvbjEwMC4GA1UEAxMnU3ltYW50ZWMgVGltZSBTdGFtcGluZyBTZXJ2aWNlcyBD
+# QSAtIEcyMB4XDTEyMTAxODAwMDAwMFoXDTIwMTIyOTIzNTk1OVowYjELMAkGA1UE
+# BhMCVVMxHTAbBgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTQwMgYDVQQDEytT
+# eW1hbnRlYyBUaW1lIFN0YW1waW5nIFNlcnZpY2VzIFNpZ25lciAtIEc0MIIBIjAN
+# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAomMLOUS4uyOnREm7Dv+h8GEKU5Ow
+# mNutLA9KxW7/hjxTVQ8VzgQ/K/2plpbZvmF5C1vJTIZ25eBDSyKV7sIrQ8Gf2Gi0
+# jkBP7oU4uRHFI/JkWPAVMm9OV6GuiKQC1yoezUvh3WPVF4kyW7BemVqonShQDhfu
+# ltthO0VRHc8SVguSR/yrrvZmPUescHLnkudfzRC5xINklBm9JYDh6NIipdC6Anqh
+# d5NbZcPuF3S8QYYq3AhMjJKMkS2ed0QfaNaodHfbDlsyi1aLM73ZY8hJnTrFxeoz
+# C9Lxoxv0i77Zs1eLO94Ep3oisiSuLsdwxb5OgyYI+wu9qU+ZCOEQKHKqzQIDAQAB
+# o4IBVzCCAVMwDAYDVR0TAQH/BAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDAO
+# BgNVHQ8BAf8EBAMCB4AwcwYIKwYBBQUHAQEEZzBlMCoGCCsGAQUFBzABhh5odHRw
+# Oi8vdHMtb2NzcC53cy5zeW1hbnRlYy5jb20wNwYIKwYBBQUHMAKGK2h0dHA6Ly90
+# cy1haWEud3Muc3ltYW50ZWMuY29tL3Rzcy1jYS1nMi5jZXIwPAYDVR0fBDUwMzAx
+# oC+gLYYraHR0cDovL3RzLWNybC53cy5zeW1hbnRlYy5jb20vdHNzLWNhLWcyLmNy
+# bDAoBgNVHREEITAfpB0wGzEZMBcGA1UEAxMQVGltZVN0YW1wLTIwNDgtMjAdBgNV
+# HQ4EFgQURsZpow5KFB7VTNpSYxc/Xja8DeYwHwYDVR0jBBgwFoAUX5r1blzMzHSa
+# 1N197z/b7EyALt0wDQYJKoZIhvcNAQEFBQADggEBAHg7tJEqAEzwj2IwN3ijhCcH
+# bxiy3iXcoNSUA6qGTiWfmkADHN3O43nLIWgG2rYytG2/9CwmYzPkSWRtDebDZw73
+# BaQ1bHyJFsbpst+y6d0gxnEPzZV03LZc3r03H0N45ni1zSgEIKOq8UvEiCmRDoDR
+# EfzdXHZuT14ORUZBbg2w6jiasTraCXEQ/Bx5tIB7rGn0/Zy2DBYr8X9bCT2bW+IW
+# yhOBbQAuOA2oKY8s4bL0WqkBrxWcLC9JG9siu8P+eJRRw4axgohd8D20UaF5Mysu
+# e7ncIAkTcetqGVvP6KUwVyyJST+5z3/Jvz4iaGNTmr1pdKzFHTx/kuDDvBzYBHUw
+# ggUwMIIEGKADAgECAhAECRgbX9W7ZnVTQ7VvlVAIMA0GCSqGSIb3DQEBCwUAMGUx
+# CzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3
+# dy5kaWdpY2VydC5jb20xJDAiBgNVBAMTG0RpZ2lDZXJ0IEFzc3VyZWQgSUQgUm9v
+# dCBDQTAeFw0xMzEwMjIxMjAwMDBaFw0yODEwMjIxMjAwMDBaMHIxCzAJBgNVBAYT
+# AlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2Vy
+# dC5jb20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNp
+# Z25pbmcgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQD407Mcfw4R
+# r2d3B9MLMUkZz9D7RZmxOttE9X/lqJ3bMtdx6nadBS63j/qSQ8Cl+YnUNxnXtqrw
+# nIal2CWsDnkoOn7p0WfTxvspJ8fTeyOU5JEjlpB3gvmhhCNmElQzUHSxKCa7JGnC
+# wlLyFGeKiUXULaGj6YgsIJWuHEqHCN8M9eJNYBi+qsSyrnAxZjNxPqxwoqvOf+l8
+# y5Kh5TsxHM/q8grkV7tKtel05iv+bMt+dDk2DZDv5LVOpKnqagqrhPOsZ061xPeM
+# 0SAlI+sIZD5SlsHyDxL0xY4PwaLoLFH3c7y9hbFig3NBggfkOItqcyDQD2RzPJ6f
+# pjOp/RnfJZPRAgMBAAGjggHNMIIByTASBgNVHRMBAf8ECDAGAQH/AgEAMA4GA1Ud
+# DwEB/wQEAwIBhjATBgNVHSUEDDAKBggrBgEFBQcDAzB5BggrBgEFBQcBAQRtMGsw
+# JAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBDBggrBgEFBQcw
+# AoY3aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElE
+# Um9vdENBLmNydDCBgQYDVR0fBHoweDA6oDigNoY0aHR0cDovL2NybDQuZGlnaWNl
+# cnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDA6oDigNoY0aHR0cDov
+# L2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDBP
+# BgNVHSAESDBGMDgGCmCGSAGG/WwAAgQwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93
+# d3cuZGlnaWNlcnQuY29tL0NQUzAKBghghkgBhv1sAzAdBgNVHQ4EFgQUWsS5eyoK
+# o6XqcQPAYPkt9mV1DlgwHwYDVR0jBBgwFoAUReuir/SSy4IxLVGLp6chnfNtyA8w
+# DQYJKoZIhvcNAQELBQADggEBAD7sDVoks/Mi0RXILHwlKXaoHV0cLToaxO8wYdd+
+# C2D9wz0PxK+L/e8q3yBVN7Dh9tGSdQ9RtG6ljlriXiSBThCk7j9xjmMOE0ut119E
+# efM2FAaK95xGTlz/kLEbBw6RFfu6r7VRwo0kriTGxycqoSkoGjpxKAI8LpGjwCUR
+# 4pwUR6F6aGivm6dcIFzZcbEMj7uo+MUSaJ/PQMtARKUT8OZkDCUIQjKyNookAv4v
+# cn4c10lFluhZHen6dGRrsutmQ9qzsIzV6Q3d9gEgzpkxYz0IGhizgZtPxpMQBvwH
+# gfqL2vmCSfdibqFT+hKUGIUukpHqaGxEMrJmoecYpJpkUe8wggU3MIIEH6ADAgEC
+# AhAPsMiIrMgO85Vt3tB8xjd+MA0GCSqGSIb3DQEBCwUAMHIxCzAJBgNVBAYTAlVT
+# MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+# b20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25p
+# bmcgQ0EwHhcNMjAwOTAzMDAwMDAwWhcNMjExMTAxMTIwMDAwWjB0MQswCQYDVQQG
+# EwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNj
+# bzEbMBkGA1UEChMSU2l0ZWNvcmUgVVNBLCBJbmMuMRswGQYDVQQDExJTaXRlY29y
+# ZSBVU0EsIEluYy4wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDNW9Sf
+# uwvfYp6wUx2V3iYTbRIuBl4NA2B3vafDi4+c18qdIAxJy0FJFqnJnyesClY5tXF+
+# vnCtGpL3chc/UeZ/nA3E4jNXr2Yq0MpUu4sZUhV9jnioEU1r3m1vIld9lRTfwFWC
+# /W8mhX5cy9sK54XZoEAJ4GIJDYIGga6Ov8vkuVO144fzVRczbfJYm3bmA0iiLnOx
+# yefQgLXSO9Ry7Dpg1yYxlhHxXYEkp2ovZPg2wDXpob68OTXfB2HGp/ALBhNaJNHy
+# IizNmXn9kzZ1if0Mk1M8/VGFgQaXB2/qmmOvp9CpYLU1JiLradz0jMFnuTvOSJna
+# WXwxqa+sKfBBOEVxAgMBAAGjggHFMIIBwTAfBgNVHSMEGDAWgBRaxLl7Kgqjpepx
+# A8Bg+S32ZXUOWDAdBgNVHQ4EFgQUPdCYeQD6kr48A2/hPw/Rdzm0FtgwDgYDVR0P
+# AQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHcGA1UdHwRwMG4wNaAzoDGG
+# L2h0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtY3MtZzEuY3Js
+# MDWgM6Axhi9odHRwOi8vY3JsNC5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLWNz
+# LWcxLmNybDBMBgNVHSAERTBDMDcGCWCGSAGG/WwDATAqMCgGCCsGAQUFBwIBFhxo
+# dHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAgGBmeBDAEEATCBhAYIKwYBBQUH
+# AQEEeDB2MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wTgYI
+# KwYBBQUHMAKGQmh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFNI
+# QTJBc3N1cmVkSURDb2RlU2lnbmluZ0NBLmNydDAMBgNVHRMBAf8EAjAAMA0GCSqG
+# SIb3DQEBCwUAA4IBAQDxmFvJ8BfAeeaATeCEpJ/wOkqlo+dxWIK/WP6BqIGo6l0a
+# Gm1T5qDXxYUsq6L0VGaGOtnSqnTx41+1+ghNbncVsF7SjYaCdqmxEqC1emb/bn/O
+# HiH8AcYwDUDafh9k0WtUYQh2iQIci5pvJpgoQY3+vSJWzxlliS1AgBYXRATWyl1/
+# 8Cg/jEZyChmBGB0RziyYyPo4TzeRxrTQdUSiWLuYL94FKj+JTINlijQA0VJPcYJD
+# 9RGClDPSQUl8COst3FMLgj6sebSLxd21A6miLw3bkJ8lH3nrSr06eMeUBiaZeDTL
+# tbe4R0W8yuDfJ9gIkUOKxYrasoSftg/5Du6ONYKOMYIELzCCBCsCAQEwgYYwcjEL
+# MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3
+# LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVkIElE
+# IENvZGUgU2lnbmluZyBDQQIQD7DIiKzIDvOVbd7QfMY3fjAJBgUrDgMCGgUAoHAw
+# EAYKKwYBBAGCNwIBDDECMAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYK
+# KwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFIuO9Oxn
+# fSZtMu1Iow+a63pL14sSMA0GCSqGSIb3DQEBAQUABIIBAKf9UZarbFDfFTmdZCtA
+# EhdEaDoRO5G/CLDOVySX8cfPnZVKGQcC8U1KBi2x5QAicAf7Q/M1RRS91zhYAJXp
+# WKAc2GJ8H40smfhPJheqtq1NbOleXwg3AqUVVI5l4aRatRc/JJnZ/KoRUhxgvjj5
+# b5Pv0TghFZV7L4E/6RsA/ARqs6dqYobgFdJY1RYPHe+wuDRkKswa3JJUY1rfDLpL
+# ZNubLoqIxuDtXLlVpED/FQq3G7rS1QF6ue2Lewyki922Y3/avQHw1Ltibo90cjHq
+# 6D0fdDMdwf8OWMUxk8/H+8UGj97Ng6C9kXk4MoKGNA+fYUJKZ0iYnn3FNOi/8Gi9
+# 1OahggILMIICBwYJKoZIhvcNAQkGMYIB+DCCAfQCAQEwcjBeMQswCQYDVQQGEwJV
+# UzEdMBsGA1UEChMUU3ltYW50ZWMgQ29ycG9yYXRpb24xMDAuBgNVBAMTJ1N5bWFu
+# dGVjIFRpbWUgU3RhbXBpbmcgU2VydmljZXMgQ0EgLSBHMgIQDs/0OMj+vzVuBNhq
+# mBsaUDAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkq
+# hkiG9w0BCQUxDxcNMjAxMTI1MDg0NTM3WjAjBgkqhkiG9w0BCQQxFgQUHCRk57qG
+# u0I2KODdHQ+fhaTCMjAwDQYJKoZIhvcNAQEBBQAEggEAO+9xH8OWly5GyA5j9uY4
+# nha8GK5h7i9Iw+/9AdALArJCoQxfgqtsPevKS4GUwZ+i2MdhhmxiSypgTMfctbAS
+# /UTfBuwWfJojvVGkw8v7kFOL/2A5tX++OjMLY+mUdBwmHCLNMISumvTFJNFy42rn
+# nyZPviVN/0m3SBkizHmC7Nzaw5NxLL9YdZnw1JV2l9B8gawOs8DLwXTMvvJ6+g4y
+# ykyUJvzLlkij+NBKq7F8uAXJXUmAB82jPPcwRtcQqHOGSwGPUTnpfnxpyTtn6zVx
+# BXmNv3uPd+xHxjGe99BDigo9/q/nN06gLHEziAAAoOuvh9gzwEYXYC2KkqIc/MKr
+# 8g==
+# SIG # End signature block
